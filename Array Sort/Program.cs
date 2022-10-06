61c59f46f7d3fba71e7e5741ef1f58870f3af6ea
PhanTien234 <minhtienp328@gmail.com>
2022-10-06 21:14:58 +0700

    Array Sort

---
+++ Array Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace okconde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so mang ma ban muon tao:");
+            int a= int.Parse(Console.ReadLine());
+           string[] mang= new string[a] ;
+           for(int i=0; i< mang.Length; i++)
+           {
+               mang[i] =Console.ReadLine();
+           }
+            for(int i=0; i< mang.Length; i++)
+           {
+              Console.WriteLine($"Danh sach cac mang: {mang[i]}" );
+           }
+           Array.Sort(mang);
+            for(int i=0; i< mang.Length; i++)
+           {
+              Console.WriteLine($"Danh sach cac mang sau khi sap xep: {mang[i]}" );
+           }
+           
+           foreach (var item in mang)
+           {
+               Console.WriteLine($"danh sach cac phan tu trong mang: {item}" );
+           }
+           Console.ReadLine();
+        }
+    }
+}
